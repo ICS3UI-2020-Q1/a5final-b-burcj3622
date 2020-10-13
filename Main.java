5d62f4980871be8415881f5f151f3cb4a70ce0d6
burcj3622 <burcj3622@wrdsb.ca>
2020-10-13 13:48:46 +0000

    odd

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,23 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+   
+   // asks user for input
+   System.out.println("Please enter the maximum number to sum to:");
+  //user input here
+    int num = input.nextInt();
+  //variable sum 
+    int sum = 0;
+   //loop to find all of the odd numbers 
+    for (int number = 1; number<= num; number = number + 2){
+    System.out.println();
+  // takes all of the odd numbers and finds the sum
+    sum = sum + number;
+    }
+  // prints to screen the sum of odd numbers
+  System.out.println("The sum of the odd numbers is " + sum);
+  
   }
 }
+
